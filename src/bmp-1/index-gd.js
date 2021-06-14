8f58c18e786603fe17d8cbfc288b00c9991497ec
Alexey Kolpakov <al.kolpak@gmail.com>
2021-06-14 18:03:24 +0300

    .

--- src/bmp-1/index-gd.js
+++ src/bmp-1/index-gd.js
@@ -3,7 +3,7 @@ const path = require('path');
 const gd = require('node-gd');
 
 const filesrc = path.join(__dirname, './EAN13code.bmp');
-const filedest = path.join(__dirname, './EAN13code.gd2.bmp');
+const filedest = path.join(__dirname, './EAN13code.gd.bmp');
 
 (async () => {
   const img = await gd.openBmp(filesrc);
